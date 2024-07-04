a9fd1c2e0279db855909e39d56d47c69400da3b8
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-07-04 16:30:23 +0300

    some changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure this matches your build output directory
+  },
+  // If your app is hosted at a subdirectory, e.g., example.com/myapp/
+  // base: '/myapp/',
+  server: {
+    port: 3000, // Change this to your preferred development port
+  },
+});
