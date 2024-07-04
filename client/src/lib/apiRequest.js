822d9282e2a5c12d9c5fc232805551ab332dd3f2
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-07-04 16:55:26 +0300

    apiRequest

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: 'https://biddershubbackend.onrender.com/api',
   withCredentials: true,
 });
 
