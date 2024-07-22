096b504db8e4b97a1c042f4161209ae0c3ec3b67
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-07-22 14:43:16 +0300

    notFound

--- bidderClient/src/components/underDeve/NotFound.jsx
+++ bidderClient/src/components/underDeve/NotFound.jsx
@@ -5,10 +5,10 @@ const NotFoundPage = () => {
   return (
     <div className="notfound-container">
       <div className="notfound-content">
-        <h1 className="notfound-title">404</h1>
-        <p className="notfound-message">
+        <h1 className="notfound-title">Welcome</h1>
+        {/* <p className="notfound-message">
           Oops! The page you are looking for does not exist.
-        </p>
+        </p> */}
         <Link to="/usersSession" className="notfound-button">
           Go Home
         </Link>
