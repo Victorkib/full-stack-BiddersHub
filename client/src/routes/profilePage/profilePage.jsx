1f582e0d43509c6edd7fcb81a37054521805cc39
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-07-25 12:47:55 +0300

    profiledataUpdate

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -14,6 +14,7 @@ import ProfileUpdatePage from '../profileUpdatePage/profileUpdatePage';
 
 function ProfilePage() {
   const data = useLoaderData();
+  console.log('profilePageData: ', data);
 
   const { updateUser, currentUser } = useContext(AuthContext);
 
