b7998da5fd781bb5912005b963b02b209f9d5ad9
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-07-25 22:53:57 +0300

    changes

--- bidderClient/vite.config.js
+++ bidderClient/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'web.config', // Path to the web.config file in your project
+          dest: '', // Destination folder, '' means root of the dist folder
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: 'dist', // Ensure this matches your build output directory
+  },
+});
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'web.config', // Path to the web.config file in your project
+          dest: '', // Destination folder, '' means root of the dist folder
+        },
+      ],
+    }),
+  ],
   build: {
     outDir: 'dist', // Ensure this matches your build output directory
   },
-  // If your app is hosted at a subdirectory, e.g., example.com/myapp/
-  // base: '/myapp/',
 });
