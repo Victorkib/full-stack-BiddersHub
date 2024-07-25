63039e3668747127013726d404e755e6ec9c80f0
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-07-25 07:27:38 +0300

    bidderNav

--- client/src/utils/Popup.jsx
+++ client/src/utils/Popup.jsx
@@ -22,9 +22,7 @@ const Popup = ({ onClose }) => {
         <h2>Bid</h2>
         <p>A brief description about bidding.</p>
         <span className={styles.link}>
-          <a href="https://bidderspagefront.onrender.com/usersSession">
-            Go to Bid Page
-          </a>
+          <a href="https://bidderspagefront.onrender.com">Go to Bid Page</a>
         </span>
       </div>
     </div>
